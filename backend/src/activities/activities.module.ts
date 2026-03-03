c3ebdb0f846e640c4d42b2a46f99a27cffdc0f3d
Ítalo Carmo <67763645+italo-carmo@users.noreply.github.com>
2026-03-03 11:22:32 -0300

    fix: exportar ActivitiesService para uso em ChecklistsModule

--- backend/src/activities/activities.module.ts
+++ backend/src/activities/activities.module.ts
@@ -7,5 +7,6 @@ import { ActivitiesService } from './activities.service';
   imports: [RbacModule],
   controllers: [ActivitiesController],
   providers: [ActivitiesService],
+  exports: [ActivitiesService],
 })
 export class ActivitiesModule {}
