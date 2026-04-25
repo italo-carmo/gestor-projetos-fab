c6e320e9022a07d781d12622f1459f34f3630dbf
Ítalo Carmo <67763645+italo-carmo@users.noreply.github.com>
2026-04-25 11:24:31 -0300

    Corrige injecao RBAC no manual CIPAVD

--- backend/src/manuals/manuals.module.ts
+++ backend/src/manuals/manuals.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RbacModule } from '../rbac/rbac.module';
 import { ManualsController } from './manuals.controller';
 
 @Module({
+  imports: [RbacModule],
   controllers: [ManualsController],
 })
 export class ManualsModule {}
